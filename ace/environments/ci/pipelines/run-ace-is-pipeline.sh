b740d7efb3a8fa5731fcd24077d1e5989fe34ac0
Irene Marquet Gómez <irene.marquet@ibm.com>
2022-12-13 15:30:49 +0100

    Deploy ace-integration-server trigger template

--- ace/environments/ci/pipelines/run-ace-is-pipeline.sh
+++ ace/environments/ci/pipelines/run-ace-is-pipeline.sh
@@ -5,10 +5,10 @@ if [ -z ${SOURCE_REPO} ]; then echo "Please set SOURCE_REPO when running script"
 
 tkn -n ci pipeline start \
   ace-integration-server \
-  --param is-source-repo-url=git@github.com:${GIT_ORG}/${SOURCE_REPO}.git \
-  --param is-infra-repo-url=git@github.com:${GIT_ORG}/ace-infra.git \
-  --param git-ops-repo-url=git@github.com:${GIT_ORG}/multi-tenancy-gitops-apps.git \
-  --param gitops-apps-repo-full-name=${GIT_ORG}/multi-tenancy-gitops-apps \
+  --param is-source-repo-url=git@github.com:irenemrqt/${SOURCE_REPO}.git \
+  --param is-infra-repo-url=git@github.com:irenemrqt/ace-infra.git \
+  --param git-ops-repo-url=git@github.com:demo-ace-deployment/multi-tenancy-gitops-apps.git \
+  --param gitops-apps-repo-full-name=demo-ace-deployment/multi-tenancy-gitops-apps \
   --workspace name=shared-workspace,claimName=ace-integration-server \
   --workspace name=ace-infra-repo-secret,secret=ace-infra-at-github \
   --workspace name=gitops-repo-secret,secret=multi-tenancy-gitops-apps-at-github \
